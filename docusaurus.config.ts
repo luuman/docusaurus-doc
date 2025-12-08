ca21d8dea081da9dfa2742299feea1b4dca85d35
zhongcheng.li <luumans@qq.com>
2025-12-08 17:30:10 +0800

    feat: docusaurus base

---
+++ docusaurus.config.ts
@@ -0,0 +1,174 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "ElectronDoc",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh",
+    locales: ["en", "zh"],
+    localeConfigs: {
+      en: {
+        htmlLang: "en-US",
+      },
+      zh: {
+        htmlLang: "zh-CN",
+      },
+    },
+  },
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          sidebarCollapsible: true,
+          sidebarCollapsed: false,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "ElectronDoc",
+      // logo: {
+      //   alt: "ElectronDoc Logo",
+      //   src: "img/logo.svg",
+      // },
+      items: [
+        // {
+        //   type: "docSidebar",
+        //   sidebarId: "tutorialSidebar",
+        //   position: "left",
+        //   label: "Doc",
+        // },
+        {
+          label: "Docs",
+          to: "docs/guide/",
+          position: "left",
+          activeBasePath: "docs",
+          // items: [
+          //   // { label: "Introduction", to: "docs/Introduction/overview" },
+          //   // { label: "Tutorials", to: "docs/tutorials/" },
+          //   // { label: "How-to Guides", to: "docs/howto/" },
+          //   // { label: "References", to: "docs/references/" },
+          //   // { label: "Explanations", to: "docs/explanations/" },
+          // ],
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        { type: "localeDropdown", position: "right" },
+        // {
+        //   href: "https://github.com/facebook/docusaurus",
+        //   label: "GitHub",
+        //   position: "right",
+        // },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        // {
+        //   title: "Docs",
+        //   items: [
+        //     {
+        //       label: "Doc",
+        //       to: "/docs/intro",
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: "Community",
+        //   items: [
+        //     {
+        //       label: "Stack Overflow",
+        //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
+        //     },
+        //     {
+        //       label: "Discord",
+        //       href: "https://discordapp.com/invite/docusaurus",
+        //     },
+        //     {
+        //       label: "X",
+        //       href: "https://x.com/docusaurus",
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: "More",
+        //   items: [
+        //     {
+        //       label: "Blog",
+        //       to: "/blog",
+        //     },
+        //     {
+        //       label: "GitHub",
+        //       href: "https://github.com/facebook/docusaurus",
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,205 @@
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: ReactNode;
+};
+
+const ElectronDocWinList: FeatureItem[] = [
+  {
+    title: "桌面端App",
+    // name: 'Matrix Windows',
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: <>Description for 桌面端App (Matrix Windows).</>,
+  },
+  // {
+  //   title: 'Electron包',
+  //   // name: 'avr_electron',
+  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  //   description: (
+  //     <>
+  //       Description for Electron包 (avr_electron).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '日志解密',
+  //   // name: 'Entry Log',
+  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 日志解密 (Entry Log).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '日志解密',
+  //   // name: 'Matrix Player',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 日志解密 (Matrix Player).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '办公工具',
+  //   // name: 'Office Excel',
+  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 办公工具 (Office Excel).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '项目',
+  //   // name: 'Almualm',
+  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 项目 (Almualm).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '桌面端SDK',
+  //   // name: 'CST SDK',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 桌面端SDK (CST SDK).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '桌面端SDK',
+  //   // name: 'Falah',
+  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 桌面端SDK (Falah).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '直播App',
+  //   // name: 'Live Webview',
+  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 直播App (Live Webview).
+  //     </>
+  //   ),
+  // },
+
+  // {
+  //   title: '聊天SDK',
+  //   // name: 'Web IM SDK',
+  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 聊天SDK (Web IM SDK).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '会议SDK',
+  //   // name: 'Web Meeting SDK',
+  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 会议SDK (Web Meeting SDK).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '风险来源平台',
+  //   // name: 'MSS',
+  //   Svg: require('@site/static/img/draw_web_mss.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 风险来源平台 (MSS).
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: '风险来源平台',
+  //   // name: 'Conf Portal',
+  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  //   description: (
+  //     <>
+  //       Description for 风险来源平台 (Conf Portal).
+  //     </>
+  //   ),
+  // },
+];
+
+const ElectronDocWebList: FeatureItem[] = [
+  {
+    title: "首页",
+    // name: 'Home',
+    Svg: require("@site/static/img/draw_web_home.svg").default,
+    description: <>Description for 首页 (Home).</>,
+  },
+  {
+    title: "会中管理",
+    // name: 'Meeting Control',
+    Svg: require("@site/static/img/draw_web_meeting.svg").default,
+    description: <>Description for 会中管理 (Meeting Control).</>,
+  },
+  {
+    title: "会前管理",
+    // name: 'Meeting',
+    Svg: require("@site/static/img/draw_web_meeting.svg").default,
+    description: <>Description for 会前管理 (Meeting).</>,
+  },
+  {
+    title: "公共模块",
+    // name: 'Module Common',
+    Svg: require("@site/static/img/draw_web_module.svg").default,
+    description: <>Description for 公共模块 (Module Common).</>,
+  },
+  {
+    title: "本地代理",
+    // name: 'Proxy Local',
+    Svg: require("@site/static/img/draw_web_proxy.svg").default,
+    description: <>Description for 本地代理 (Proxy Local).</>,
+  },
+];
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {ElectronDocWinList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+        <div className="row">
+          {ElectronDocWebList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
